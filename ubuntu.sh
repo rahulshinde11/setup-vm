089baad6e379fa2e1bae7746a641523a37f7576e
Rahul Shinde <44699277+rahulshinde11@users.noreply.github.com>
2022-04-18 01:44:13 +0530

    update after adding docker repo

--- ubuntu.sh
+++ ubuntu.sh
@@ -19,6 +19,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
+sudo apt-get --yes update
+
 sudo apt-get --yes install docker-ce docker-ce-cli containerd.io
 
 
