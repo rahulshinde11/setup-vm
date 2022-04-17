c1fa82ab22bff62a3cce24093a60dfbb4277dbb5
Rahul Shinde <44699277+rahulshinde11@users.noreply.github.com>
2022-04-18 01:19:13 +0530

    added ubuntu script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # setup-vm
 Sets up ubuntu VM with docker and docker-compose
+
---
+++ ubuntu.sh
@@ -0,0 +1,31 @@
+
+# Sets up ubuntu VM with docker and docker-compose
+
+# os updates
+sudo apt-get --yes update
+sudo apt-get --yes --force-yes upgrade
+
+# docker
+sudo apt-get --yes install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+sudo apt-get --yes install docker-ce docker-ce-cli containerd.io
+
+
+sudo usermod -aG docker $USER
+
+
+# Docker compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
